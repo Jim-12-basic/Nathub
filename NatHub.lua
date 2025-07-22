00e438dfd9fc32ad13f37f96cf4d6b46269864e2
TIMBERWOLVES <masteredge454@gmail.com>
2025-07-22 18:17:25 +0800

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/9qAX0PFWgX/0", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/OJgWO9923ZRSo0Is/raw", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
