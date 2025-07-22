c5cfdfd91fe8ac86d1de1ffbd08d854017982129
TIMBERWOLVES <masteredge454@gmail.com>
2025-07-22 09:40:41 +0800

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/9qAX0PFWgX/0", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
